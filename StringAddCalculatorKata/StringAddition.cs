cd9ca37bcc774d9a61f6e474817443b33c1be403
Mohammad <mohammad1162002@gmail.com>
2023-03-22 18:51:22 +0200

    fix on step3

--- StringAddCalculatorKata/StringAddition.cs
+++ StringAddCalculatorKata/StringAddition.cs
@@ -1,13 +1,16 @@
-﻿namespace StringAddCalculatorKata
+﻿using System.Linq;
+
+namespace StringAddCalculatorKata
 {
     public static class StringAddition
     {
-        public static int AddNumbersInString(this string Number)
+        public static int AddNumbersInString(this string Input)
         {
-            if (string.IsNullOrWhiteSpace(Number))
+            if (string.IsNullOrWhiteSpace(Input))
                 return 0;
-
-            return Number.Split(",")
+            if(Input.Contains(",\n")||Input.Contains("\n,"))
+                throw new ArgumentException("Not Valid Separator", nameof(Input));
+            return Input.Split(',','\n')
                          .Aggregate(0, (total, value) =>
                              string.IsNullOrWhiteSpace(value) ?
                                 total : total + Convert.ToInt32(value));
