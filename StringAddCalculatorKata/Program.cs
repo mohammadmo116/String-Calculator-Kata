c9824f55d9c765fbcfd5ee2541fa67a08d8bf4e4
Mohammad <mohammad1162002@gmail.com>
2023-03-22 18:35:47 +0200

    step 3 - handle new lines between numbers

--- StringAddCalculatorKata/Program.cs
+++ StringAddCalculatorKata/Program.cs
@@ -1,4 +1,11 @@
-﻿
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+public class Test
+{
 
+    public int add(string Number)
+    {
+        return Convert.ToInt32(Number);   
 
-Console.WriteLine("Hello World!");
+    }
+}
