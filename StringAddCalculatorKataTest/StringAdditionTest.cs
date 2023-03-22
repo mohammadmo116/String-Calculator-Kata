8d75295df171bdf4fa73a6c7186dacf41afd00c4
Mohammad <mohammad1162002@gmail.com>
2023-03-22 21:20:55 +0200

    add comments

--- StringAddCalculatorKataTest/StringAdditionTest.cs
+++ StringAddCalculatorKataTest/StringAdditionTest.cs
@@ -12,14 +12,18 @@ namespace StringAddCalculatorKataTest
         }
 
         [Theory]
+        //step1
         [InlineData(" ", 0)]
         [InlineData("", 0)]
         [InlineData("1", 1)]
+        //step2
         [InlineData("15,9,,7", 31)]
         [InlineData("15,9, ,8, ", 32)]
+        //step3
         [InlineData("2\n12\n4", 18)]
         [InlineData("2\n12\n", 14)]
         [InlineData("1\n2,4", 7)]
+        //step4
         [InlineData("//;\n1;2", 3)]
         [InlineData("//t\n1t12", 13)]
         [InlineData("//?\n1?3?7", 11)]  
@@ -33,8 +37,10 @@ namespace StringAddCalculatorKataTest
         }
         
         [Theory]
+        //step3
         [InlineData("2\n12,\n1")]
         [InlineData("2\n12\n,4\n")]
+        //step4
         [InlineData("//?\n1?3?\n7")]
         [InlineData("//?\n1?3\n?7")]
         public void HandleNotValidInputTest(string Input)
