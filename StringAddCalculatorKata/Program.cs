ce7c9299f788a82d48d15237b0ae0f351ea59b07
Mohammad <mohammad1162002@gmail.com>
2023-03-22 18:43:18 +0200

    step3 -  handle new lines between numbers

--- StringAddCalculatorKata/Program.cs
+++ StringAddCalculatorKata/Program.cs
@@ -1,11 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-public class Test
-{
-
-    public int add(string Number)
-    {
-        return Convert.ToInt32(Number);   
-
-    }
-}
---
+++ StringAddCalculatorKata/StringAddition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAddCalculatorKata
+{
+    public static class StringAddition
+    {
+        public static int AddNumbersInString(this string Input)
+        {
+            if (string.IsNullOrWhiteSpace(Input))
+                return 0;
+            if(Input.Contains(",\n")|| Input.Contains("\n,"))
+                throw new ArgumentException("Not Valid Separator", nameof(Input));
+            return Input.Split(',','\n')
+                         .Aggregate(0, (total, value) =>
+                             string.IsNullOrWhiteSpace(value) ?
+                                total : total + Convert.ToInt32(value));
+
+        }
+    }
+}
